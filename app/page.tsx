8f47de40aee463e272051b4a0a4579928cbe329a
Tariq Zaitoun <tariq.s.zaitoun@gmail.com>
2024-08-23 11:30:49 -0400

    Added background image and fixed bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/chat");
+  }, [router]);
+
   return (
     <>Hello</>
   );
